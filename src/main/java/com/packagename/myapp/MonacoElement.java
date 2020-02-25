01a7edca33b356aa982a509709122afc11d82e58
Dima Gerasym <gerasym@Dimas-MacBook-Pro.local>
2020-02-25 17:31:05 +0100

    using the monaco-element as webcomponent inside an html tag

--- src/main/java/com/packagename/myapp/MonacoElement.java
+++ src/main/java/com/packagename/myapp/MonacoElement.java
@@ -9,12 +9,12 @@ import com.vaadin.flow.component.polymertemplate.EventHandler;
 
 import java.util.function.Consumer;
 
-@Tag("monaco-element")
+@Tag("demo-element")
 //doesn't work properly - doesnt install dependencies, no idea why.
 //better just run npm -i monaco-editor --save
 //@NpmPackage(value = "monaco-editor", version = "0.19.3")
 //@NpmPackage(value = "monaco-editor-webpack-plugin", version = "1.8.2")
-@JsModule("./src/monaco-element.js")
+@JsModule("./src/monacoInsideHtml.js")
 public class MonacoElement extends AbstractSinglePropertyField<MonacoElement,String> {
     public MonacoElement() {
         super("value","",false);
@@ -23,20 +23,6 @@ public class MonacoElement extends AbstractSinglePropertyField<MonacoElement,Str
         getElement().getStyle().set("width", "100%");
 
     }
-//
-//    @EventHandler
-//    public void handleEvent(){
-//        System.out.println("something has changed!");
-//    }
-//    @Override
-//    public void setValue(String value) {
-//        getElement().callJsFunction("setValue", value, 1);
-//    }
-//
-//    public void getValue(Consumer<String> consumer) {
-//        getElement().callJsFunction("getValue").then(result -> {
-//            consumer.accept(result.toJson());
-//        });
-//    }
+
 }
 
